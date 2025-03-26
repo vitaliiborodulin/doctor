3be63dd4ef0cc9a7544f24a4d3a1b8b3687223d1
vitaliiborodulin <vitaliiborodulin@yandex.ru>
2025-03-26 16:02:35 +0300

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -51,33 +51,20 @@ $(function() {
     // });
     $('input[type="tel"]').mask("+7 (999) 999-99-99");
     // $('input[name="birth"]').mask("99.99.9999");
-    $('.accordion__item:first-child').find('.accordion__header').addClass('active');
-    $('.accordion__item:first-child').find('.accordion__body').show();
+    // $('.accordion__item:first-child').find('.accordion__header').addClass('active');
+    // $('.accordion__item:first-child').find('.accordion__body').show();
     
-    $('.accordion__header').on('click', function () {
-        const accBody = $(this).next();
+    $('.question').on('click', function () {
+        const answer = $(this).next();
     
         if (!$(this).hasClass('active')){
-            $('.accordion__header').removeClass('active');
-            $('.accordion__body').slideUp(300);
+            $('.question').removeClass('active');
+            $('.answer').slideUp(300);
             $(this).addClass('active');
-            accBody.slideDown(300);
+            answer.slideDown(300);
         } else {
             $(this).removeClass('active');
-            accBody.slideUp(300);
-        }
-    });
-    
-    
-    $('.filter-btn').on('click', function () {
-        const filter = $('.filter');
-    
-        if (!filter.hasClass('active')){
-            filter.addClass('active');
-            filter.slideDown(300);
-        } else {
-            filter.removeClass('active');
-            filter.slideUp(300);
+            answer.slideUp(300);
         }
     });
     // $('[data-fancybox]').fancybox({
