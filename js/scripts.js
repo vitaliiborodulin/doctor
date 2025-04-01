3ee8afbb0d34588d5bc3fb9d95ad5ee9549a1816
vitaliiborodulin <vitaliiborodulin@yandex.ru>
2025-04-01 12:31:45 +0300

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -7,15 +7,15 @@ $(function() {
     burger.on('click', function(e) {
         e.preventDefault();
         burger.toggleClass('burger-close');
-        // menu.toggleClass('open').toggle(300);
-        // $('html, body').toggleClass('overflow');
+        menu.toggleClass('open').toggle(300);
+        $('html, body').toggleClass('overflow');
     });
     
     $(document).keyup(function(e) {
         if (e.key === "Escape" || e.keyCode === 27) {
-            // menu.removeClass('open');
+            menu.removeClass('open');
             burger.removeClass('burger-close');
-            // $('html, body').removeClass('overflow');
+            $('html, body').removeClass('overflow');
         }
     });
     
@@ -51,6 +51,37 @@ $(function() {
     // });
     // $('input[type="tel"]').mask("+7 (999) 999-99-99");
     // $('input[name="birth"]').mask("99.99.9999");
+    var num = $(".number");
+    num.each(function(indx, el) {
+        var max = $(el).data("max");
+        var duration = 2000;
+        var visibility = checkViewport(el);
+        $(el).on("animeNum", function() {
+            $({n: 0}).animate({n: max}, {
+                easing: "linear",
+                duration: duration,
+                step: function(now, fx) {
+                    $(el).html(now | 0)
+                }
+            })
+        }).data("visibility", visibility);
+        visibility && $(el).trigger("animeNum")
+    });
+    
+    function checkViewport(el) {
+    var H = document.documentElement.clientHeight,
+        h = el.scrollHeight,
+        pos = el.getBoundingClientRect();
+        return pos.top + h > 0 && pos.bottom - h < H
+    }
+    $(window).scroll(function() {
+        num.each(function(indx, el) {
+            var visibility = checkViewport(el);
+            el = $(el);
+            var old = el.data("visibility");
+            old != visibility && el.data("visibility", visibility) && !old && el.trigger("animeNum")
+        })
+    })
     // $('.accordion__item:first-child').find('.accordion__header').addClass('active');
     // $('.accordion__item:first-child').find('.accordion__body').show();
     
